a64596bbeea8f3d9eaef1aac30978963a800e6bc
pascual777 <133131361+pascual777@users.noreply.github.com>
2024-06-11 00:04:27 +0200

    Actualizar settings.py

--- Paraty/settings.py
+++ Paraty/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "Paraty",
         "USER": "postgres",
-        "PASSWORD": "Telocres7@1",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
